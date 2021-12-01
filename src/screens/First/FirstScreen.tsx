fa7cbadc62bce302bdee08db80cc567d722755ce
Ramiro Fuentes <raff2005@gmail.com>
2021-12-01 09:31:44 -0600

    Using useState hook

--- src/screens/First/FirstScreen.tsx
+++ src/screens/First/FirstScreen.tsx
@@ -1,13 +1,13 @@
+import * as React from 'react';
 import {NativeStackScreenProps} from '@react-navigation/native-stack';
 import {RootStackParamList} from '../navigation/Types';
-import * as React from 'react';
 import styles from '../../styles/styles';
 import {View, Text, Button, TextInput} from 'react-native';
 
 type Props = NativeStackScreenProps<RootStackParamList, 'FirstScreen'>;
 
 const FirstScreen = ({navigation}: Props) => {
-  let paramValue: string = '';
+  const [textValue, setTextValue] = React.useState('');
 
   return (
     <View style={styles.view}>
@@ -15,14 +15,14 @@ const FirstScreen = ({navigation}: Props) => {
       <TextInput
         style={styles.input}
         onChangeText={(value: string) => {
-          paramValue = value;
+          setTextValue(value);
         }}
       />
       <Button
         title={'Navigate to second'}
         onPress={() =>
           navigation.push('SecondScreen', {
-            firstParam: paramValue,
+            firstParam: textValue,
           })
         }
       />
